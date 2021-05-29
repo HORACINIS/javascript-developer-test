d0c1c6ec5527e4d44fc87bcf1b743eef4da8e478
Horacinis <horacinis@gmail.com>
2021-05-29 12:03:11 +1000

    test passed

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,22 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+
+  const results = [];
+
+  urls.forEach(async url => {
+    let result;
+    const data = await httpGet(url)
+
+    if (data.status === 200) result = { 'Arnie Quote': JSON.parse(data.body).message };
+    if (data.status !== 200) result = { 'FAILURE': JSON.parse(data.body).message };
+
+    results.push(result);
+  });
+
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(results), 200);
+  });
 };
 
 module.exports = {
